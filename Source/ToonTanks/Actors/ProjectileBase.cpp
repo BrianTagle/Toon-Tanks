a7c163af559eef418f251e9f0a5ccd97312f29f8
Brian Tagle <taglebrian@gmail.com>
2020-10-31 11:02:40 -0700

    full toon tank game commit

---
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -0,0 +1,62 @@
+#include "ProjectileBase.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+	
+	RootComponent = ProjectileMesh;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->InitialSpeed = MovementSpeed;
+	ProjectileMovement->MaxSpeed = MovementSpeed;
+
+	ParticleTrail = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleTrail"));
+	ParticleTrail->SetupAttachment(RootComponent);
+
+
+	InitialLifeSpan = 3.0f;
+}
+
+
+// Called when the game starts or when spawned
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
+	UGameplayStatics::PlaySoundAtLocation(this, LaunchSound, GetActorLocation());
+	
+}
+
+void AProjectileBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) 
+{
+	// Try to get a reference to the owning class.
+	AActor* MyOwner = GetOwner();
+	// If for some reason we can't get a valid reference, return as we need to check against the owner. 
+	if(!MyOwner)
+	{
+		return;
+	}
+	// If the other ISN'T self OR Owner AND exists, then apply damage. 
+	if(OtherActor && OtherActor != this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner->GetInstigatorController(), this, DamageType);
+		UGameplayStatics::SpawnEmitterAtLocation(this, HitParticle, GetActorLocation());
+		UGameplayStatics::PlaySoundAtLocation(this, LaunchSound, GetActorLocation());
+		GetWorld()->GetFirstPlayerController()->ClientPlayCameraShake(HitShake);
+		Destroy();
+
+	}
+
+	// Play a bunch of effects here during the polish phase. - TODO
+
+	Destroy();
+}
+
---
+++ Source/ToonTanks/GameModes/TankGameModeBase.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankGameModeBase.generated.h"
+
+class APawnTurret;
+class APawnTank;
+class APlayerControllerBase;
+
+
+UCLASS()
+class TOONTANKS_API ATankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+	void ActorDied(AActor* DeadActor);
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Game Loop")
+	int32 StartDelay = 3;
+
+	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameStart();
+	UFUNCTION(BlueprintImplementableEvent)
+	void GameOver(bool PlayerWon);
+
+private:
+	
+	void HandleGameStart();
+	void HandleGameOver(bool PlayerWon);
+	
+	APlayerControllerBase* PlayerControllerRef;
+	APawnTank *PlayerTank;
+	int32 TargetTurrets = 0;
+
+	int32 GetTargetTurretCount();
+
+
+
+	
+};
